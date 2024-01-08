1c63bf4eb2c799bf4774b0a1466bd7378e4bdead
Osami Yamamoto <osami@meijo-u.ac.jp>
2024-01-08 22:22:10 +0900

    put several languages to hello.go

--- hello/hello.go
+++ hello/hello.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+	msgs := []string{"Hello, World!", "こんにちは，世界",
+		"Olá Mundo", "안녕하세요, 월드!",
+		"Bonjour le monde!", "你好世界！"}
+	for _, m := range msgs {
+		fmt.Println(m)
+	}
 }
