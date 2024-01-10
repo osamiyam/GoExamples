947db15485c549572cc60aa238a712e5a83cf7d9
Osami Yamamoto <39205109+osamiyam@users.noreply.github.com>
2024-01-10 17:16:20 +0900

    add another benchmark

--- multithread/sample.go
+++ multithread/sample.go
@@ -23,20 +23,31 @@ func work1() {
 	ch := make(chan int)
 	go fibwork(fibN - 2, ch)
 	go fibwork(fibN - 1, ch)
-	fmt.Printf("fib(%d) = %d\n", fibN, <-ch + <-ch) 
+	res1, res2 := <-ch, <-ch
+	fmt.Printf("fib(%d) = %d\n", fibN, res1 + res2)
 }
 
 func work2() {
 	fmt.Printf("fib(%d) = %d\n", fibN, fib(fibN))
 }
 
+func work3() {
+	ch := make(chan int)
+	go fibwork(fibN - 2, ch)
+	res := fib(fibN - 1)
+	fmt.Printf("fib(%d) = %d\n", fibN, res + <-ch) 
+}
+
 func main() {
 	fmt.Printf("\n** Computing fib(%d) **\n\n", fibN)
 	start := time.Now()
 	work1()
-	fmt.Println("Two threads: ", time.Since(start))
+	fmt.Println("Two threads & main: ", time.Since(start))
 	start = time.Now()
 	work2()
 	fmt.Println("Single thread: ", time.Since(start))
+	start = time.Now()
+	work3()
+	fmt.Println("One thread & main: ", time.Since(start))
 	fmt.Println()
 }
