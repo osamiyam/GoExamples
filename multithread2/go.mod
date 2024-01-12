ef55424d8643d9ecf93464f674a8c50b84efb71f
Osami Yamamoto <39205109+osamiyam@users.noreply.github.com>
2024-01-12 12:36:51 +0900

    add multithread2

---
+++ multithread2/go.mod
@@ -0,0 +1,3 @@
+module multithread2
+
+go 1.21.5
---
+++ multithread2/sample.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+const (
+	N = 100
+	nthreads = 10
+)
+
+func producer(ch chan int) {
+	for i := 1; i <= N; i++ {
+		ch <- i
+	}
+	for i := 0; i < nthreads; i++ {
+		ch <- 0
+	}
+
+}
+
+func consumer(ch chan int, no int, wg *sync.WaitGroup) {
+	defer wg.Done()
+	data := [] int {}
+	for {
+		i := <-ch
+		if i == 0 {
+			break
+		}
+		fmt.Printf("This is Process %d. I got %d.\n", no, i)
+		data = append(data, i)
+	}
+	fmt.Printf("Process %d: All numbers I've recieved are %d.\n", no, data)
+}
+
+func main() {
+	var wg sync.WaitGroup
+	ch := make(chan int)
+	for i := 0; i < nthreads; i++ {
+		wg.Add(1); go consumer(ch, i, &wg)
+	}
+	producer(ch)
+	wg.Wait()
+}
